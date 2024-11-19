c01122ed74f48ce330ab0350039b46d0897adeb4
rotradam <meloneusk@gmail.com>
2024-11-19 21:51:34 +0100

    base nextjs set up for develop

---
+++ charts/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'assets.coingecko.com',
+      'static.binance.com'
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig; 
